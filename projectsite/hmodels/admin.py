50ba058e0e6b1d37763e0e17fae477c34a7db6b0
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2025-08-28 10:31:19 +0800

    admin.py enchancement

--- projectsite/hmodels/admin.py
+++ projectsite/hmodels/admin.py
@@ -12,20 +12,27 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("category_name",)
     search_fields = ("category_name",)
 
-admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ("task_title", "task_status", "task_deadline", "task_priority", "task_category",)
-    list_filter = ("task_status", "task_priority", "task_category",)
-    search_fields = ("task_title", "task_description",)
-
-admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
+class NoteInline(admin.StackedInline):
+    model = Note
+    extra = 1
+    fields = ("note_content", "created_at",)
+    readonly_fields = ("created_at",)
     list_display = ("note_task", "note_content", "created_at",)
     list_filter = ("created_at",)
     search_fields = ("note_content",)
 
-admin.register(SubTask)
-class SubTaskAdmin(admin.ModelAdmin):
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+    fields = ("sub_title", "sub_status",)
+    show_change_link = True
     list_display = ("sub_title", "sub_status", "sub_parent_task",)
     list_filter = ("sub_status",)
     search_fields = ("sub_title",)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("task_title", "task_status", "task_deadline", "task_priority", "task_category",)
+    list_filter = ("task_status", "task_priority", "task_category",)
+    search_fields = ("task_title", "task_description",)
+    inlines = [SubTaskInline, NoteInline]
